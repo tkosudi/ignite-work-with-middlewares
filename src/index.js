6cde625399a939f44222330af9cc32f3553d78d0
tkosudi <tkosudi@gmail.com>
2022-09-05 17:00:25 -0300

    all middlewares were built

--- src/index.js
+++ src/index.js
@@ -10,19 +10,58 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+  if (!user) {
+    return response.status(404).json({error: 'Usuário não nexiste!'})
+  }
+
+  request.user = user
+  next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request
+  if (user.pro === false && user.todos.length >= 10) {
+    return response.status(403)
+  }
+
+  next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.find(user => user.username === username)
+  if (!user) {
+    return response.status(404).json({error: 'Usuário não nexiste!'})
+  }
+  
+  if (!validate(id)) {
+    return response.status(400).json({error: 'Esse ID não é valido!'})
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo não existe!'})
+  }
+
+  request.user = user
+  request.todo = todo
+  next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+  const user = users.find(user => user.id === id)
+
+  if (!user) {
+    return response.status(404).json({ error: 'Usuário não nexiste!'})
+  }
+
+  request.user = user
+  next()
 }
 
 app.post('/users', (request, response) => {
